6bf4af5312dfc8445ea1761d9a39c3f787b2b8a8
chandrakant685 <chandrakant685@gmail.com>
2018-10-18 11:32:51 +0530

    Add files via upload

---
+++ AccessTypeCell.swift
@@ -0,0 +1,25 @@
+//
+//  BuyerListCell.swift
+//  PEMS
+//
+//  Created by Unify Systems on 08/10/18.
+//  Copyright © 2018 Unify Systems. All rights reserved.
+//
+
+import UIKit
+
+class BuyerListCell: UITableViewCell {
+
+    @IBOutlet var viewImage: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
